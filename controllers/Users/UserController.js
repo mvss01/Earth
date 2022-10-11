5a323a1a3319634fb1493619f6356c1af0e62ae0
mvss01 <marcosvictorss@hotmail.com>
2022-10-10 22:57:41 -0300

    V1.0.2
    Elaboração do front-end

--- controllers/Users/UserController.js
+++ controllers/Users/UserController.js
@@ -3,21 +3,21 @@ const router = express.Router();
 const User = require("./user");
 
 router.get('/login', (req, res) =>{
-    res.render('admin/users/login')
+    res.render('admin/users/login', {render: 0})
 })
 router.get('/users/perfil', (req, res) =>{
-    res.render('admin/users/perfil')
+    res.render('admin/users/perfil', {render: 1})
 })
 
-router.get('/users/register', (req, res) =>{
-    res.render('admin/users/register')
+router.get('/', (req, res) =>{
+    res.render('admin/home/home', {render: 0})
 })
 router.post('/users/save', (req, res) =>{
 
 })
 
 router.get('/users/edit/:user', (req, res) =>{
-    res.render('admin/users/edit')
+    res.render('admin/users/edit', {render: 1})
 })
 router.post('/users/update/:user', (req, res) =>{
 
--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('earth',/*'tecnofluid','t3cn0flu1d'*/'root', '2m0m2m1m',{
+const connection = new Sequelize('earth', 'root', '',{
     host: /*'portalsims.com'*/ 'localhost',
     dialect: 'mysql',
               timezone: '-03:00',
